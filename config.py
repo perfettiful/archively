e69f6e880996903098dcd31d4023f4c786867302
Nathan Perfetti <nathanperfetti@gmail.com>
2025-01-24 22:50:54 -0600

    adjust handling of env vars

--- config.py
+++ config.py
@@ -1,14 +1,14 @@
 import os
 
-DB_NAME = os.environ.get("DB_NAME", "link_archive")
-DB_USER = os.environ.get("DB_USER", "link_user")
-DB_PASSWORD = os.environ.get("DB_PASSWORD", "link_password")
-DB_HOST = os.environ.get("DB_HOST", "localhost")
-DB_PORT = os.environ.get("DB_PORT", "5432")
+DB_NAME = os.environ.get("DB_NAME")
+DB_USER = os.environ.get("DB_USER")
+DB_PASSWORD = os.environ.get("DB_PASSWORD") 
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT")
 
 class Config:
     SQLALCHEMY_DATABASE_URI = (
         f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get("SECRET_KEY", "super-secret-key")
+    SECRET_KEY = os.environ.get("SECRET_KEY")
